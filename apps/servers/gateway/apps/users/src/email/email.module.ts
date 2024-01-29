7ea393ec712bb3b9ce727ac846c714c019270da4
DoNhatNam1 <namnhatvt2595@gmail.com>
2024-01-29 13:57:18 +0700

    refactor: :art: fix email file

--- apps/servers/gateway/apps/users/src/email/email.module.ts
+++ apps/servers/gateway/apps/users/src/email/email.module.ts
@@ -22,7 +22,7 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
           from: 'Becodemy',
         },
         template: {
-          dir: join(__dirname, '../../../../servers/email-templates'),
+          dir: join(__dirname, '../../../../gateway/email-templates'),
           adapter: new EjsAdapter(),
           options: {
             strict: false,
